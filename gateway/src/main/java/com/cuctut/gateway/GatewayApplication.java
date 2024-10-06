fb59c3be6f0f913e549a2006c0df919bfc9b525c
Lin Song <2393300434@qq.com>
2024-10-07 03:18:00 +0800

    新增gateway模块

---
+++ gateway/src/main/java/com/cuctut/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.cuctut.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关启动类
+ *
+ * @author cuctut
+ * @since 2024/10/07
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
